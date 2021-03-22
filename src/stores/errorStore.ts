3bf0c1bd66aca36e351e4e2c4c1f3a099e14c1b5
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2021-03-22 22:32:21 +0100

    created error store

---
+++ src/stores/errorStore.ts
@@ -0,0 +1,37 @@
+import { atom, useRecoilState } from "recoil"
+
+
+interface IError {
+    message: string
+    type: "ERROR" | "WARNING" | "INFORMATION"
+}
+
+export type IErrorStore = {
+    error?: IError
+    display: boolean
+}
+
+export const errorStore = atom<IErrorStore>({
+	key: "error-store",
+	default: {
+		display: false
+	}
+})
+
+export function useErrorAction() {
+	const [, setError] = useRecoilState(errorStore)
+
+	return {
+		toggleError(error: IError, time?: number) {
+			setError({
+				display: true,
+				error
+			})
+			setTimeout(() => {
+				setError({
+					display: false
+				})
+			}, time || 3000)
+		}
+	}
+}
