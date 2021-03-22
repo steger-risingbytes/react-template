1c1e5c5f6f7f1d4b1f69e278a00720efa1ff570f
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2021-03-22 21:31:00 +0100

    created site

---
+++ src/components/sites/NotFoundSite.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+export function NotFoundSite() {
+	return (
+		<h1
+			style={{
+				margin: 24,
+				textAlign: "center"
+			}}
+		>
+            404 - Seite nicht gefunden
+		</h1>
+	)
+}
