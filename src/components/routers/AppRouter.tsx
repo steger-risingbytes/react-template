62ec7e6282e0d69d1e2229d56132088cacd880ac
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2021-03-22 20:43:38 +0100

    created app router

---
+++ src/components/routers/AppRouter.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { BrowserRouter, Switch } from "react-router-dom"
+
+export function AppRouter() {
+	return (
+		<BrowserRouter>
+			<Switch>
+                
+                
+
+			</Switch>
+		</BrowserRouter>
+	)
+}
