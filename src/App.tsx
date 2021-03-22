8fa146e04b9fc19a7e4a9ce402f63125fece5dd0
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2021-03-22 21:07:50 +0100

    added approuter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import * as React from "react"
 import { RecoilRoot } from "recoil"
+import { AppRouter } from "./components/routers/AppRouter"
 
 export function App() {
 	return (
 		<RecoilRoot>
-			<div>Hello World</div>
+			<AppRouter />
 		</RecoilRoot>
 	)
 }
