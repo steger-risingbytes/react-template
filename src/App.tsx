9cd5d1caf2fd03eeea5e1126260221e5ba409a31
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2021-03-22 21:34:19 +0100

    applied mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { MuiThemeProvider } from "@material-ui/core"
 import * as React from "react"
 import { RecoilRoot } from "recoil"
 import { AppRouter } from "./components/routers/AppRouter"
+import { theme } from "./config/theme"
 import "./index.css"
 
 export function App() {
 	return (
 		<RecoilRoot>
-			<AppRouter />
+			<MuiThemeProvider theme={theme}>
+				<AppRouter />
+			</MuiThemeProvider>
 		</RecoilRoot>
 	)
 }
