c29398f3103d6460da0b56a440651e03cc59cd0b
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2021-03-22 21:30:50 +0100

    imported stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { RecoilRoot } from "recoil"
 import { AppRouter } from "./components/routers/AppRouter"
+import "./index.css"
 
 export function App() {
 	return (
