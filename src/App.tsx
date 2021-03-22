7f0f283b372c212183f46ca7ea5749cd2f6bde0a
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2021-03-22 22:33:26 +0100

    applied snackbar to the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { MuiThemeProvider } from "@material-ui/core"
 import * as React from "react"
 import { RecoilRoot } from "recoil"
 import { AppRouter } from "./components/routers/AppRouter"
+import { ErrorSnackbar } from "./components/ui/ErrorSnackBar"
 import { theme } from "./config/theme"
 import "./index.css"
 
@@ -10,6 +11,7 @@ export function App() {
 		<RecoilRoot>
 			<MuiThemeProvider theme={theme}>
 				<AppRouter />
+				<ErrorSnackbar />
 			</MuiThemeProvider>
 		</RecoilRoot>
 	)
