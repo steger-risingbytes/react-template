ea1fbbe05d6d45d0338cedb070fb272f77241c80
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2021-03-23 10:39:26 +0100

    created custom select field

---
+++ src/components/ui/CustomSelectField.tsx
@@ -0,0 +1,46 @@
+import { FormControl, FormHelperText, InputLabel, MenuItem, Select, SelectProps } from "@material-ui/core"
+import { FieldInputProps } from "formik"
+import * as React from "react"
+
+type IProps = SelectProps & {
+    errorMessage?: string;
+    field: FieldInputProps<string>;
+    isTouched?: boolean;
+    selectOptions: string[]
+};
+
+export const CustomSelectField = ({
+	style,
+	label,
+	required,
+	errorMessage,
+	field,
+	isTouched,
+	selectOptions,
+	...props
+}: IProps) => {
+	const showError = isTouched && !!errorMessage
+
+	return (
+		<FormControl error={showError} style={style} fullWidth>
+			<InputLabel>{required ? `${label} *` : label}</InputLabel>
+			<Select
+				value={field.value}
+				name={field.name}
+				onChange={field.onChange}
+				onBlur={field.onBlur}
+				fullWidth
+				error={showError}
+				margin="dense" 
+				aria-label={props["aria-label"]}
+			>
+				{selectOptions.map((value, key) => {
+					return (
+						<MenuItem value={value} key={key}>{value}</MenuItem>
+					)
+				})}
+			</Select>
+			<FormHelperText>{showError && errorMessage}</FormHelperText>
+		</FormControl>
+	)
+}
